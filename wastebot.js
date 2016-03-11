c478564499fea05d421ab2d79144ce54e27a001c
th2 <github2@hehmann.com>
2016-03-11 18:20:01 +0100

    split messaging loop in separate functions for message generation and response

--- wastebot.js
+++ wastebot.js
@@ -126,143 +126,151 @@ function getUpdates (start) {
   }
 
   query(url, function (updates) {
-    // console.log(date.toISOString()+' updates:'+util.inspect(updates));
-
     if (updates !== 'error') {
-      // respond to incomming messages
-      for (var i in updates.result) {
-        var m = updates.result[i]
-
-        if (m.update_id > updateID) {
-          updateID = m.update_id
-        }
-        var message = m.message
-
-        log(message.chat.id, message.date + ' ' + message.from.first_name +
-          '(' + message.from.id + ')@' + message.chat.first_name + '(' +
-          message.chat.id + '): ' + message.text)
-
-        if (m.message.text === undefined) {
-          log(message.chat.id, util.inspect(message))
-          sendMessage(m.message.chat.id, 'Sorry, I could not understand message.')
-        } else if (m.message.text === '/kalender') {
-          sendMessage(m.message.chat.id, 'Die Termine der nächsten zwei Wochen sind:\n' + printList(getNextDays(14)))
-        } else if (m.message.text === '/start') {
-          if (users[m.message.chat.id]) {
-            sendMessage(m.message.chat.id, 'Du stehst bereits auf der Nutzerliste. Um von der Liste entfernt zu werden sende /stop')
-          } else {
-            sendMessage(m.message.chat.id, 'Du wurdest zur Nutzerliste hinzugefügt.')
-            fs.appendFile('config/wastebot.users', m.message.chat.id + '\n', function (err) {
-              if (err !== null) {
-                console.log('error3:' + err)
-              }
-            })
-            fs.writeFile('config/' + m.message.chat.id + '.username', m.message.chat.first_name, function (err) {
-              if (err !== null) {
-                console.log('error4:' + err)
-              }
-            })
-
-            var newUser = {}
-            newUser.name = m.message.chat.first_name
-            users[m.message.chat.id] = newUser
-          }
-        } else if (m.message.text === '/stop') {
-          if (users[m.message.chat.id]) {
-            sendMessage(m.message.chat.id, 'Du wurdest von der Nutzerliste entfernt. Um wieder als Nutzer hinzugefügt zu werden sende /start')
-            var usersNew = {}
-            for (var a in users) {
-              if (a !== m.message.chat.id) {
-                usersNew[a] = users[a]
-              }
-            }
-            users = usersNew
-
-            var userText = ''
-            for (var a2 in users) {
-              userText += a2 + '\n'
-            }
-
-            fs.writeFile('config/wastebot.users', userText, function (err) {
-              console.log('error5:' + err)
-            })
-          } else {
-            sendMessage(m.message.chat.id, 'Du stehst nicht auf der Nutzerliste. Um als Nutzer hinzugefügt zu werden sende /start')
-          }
-        } else if (m.message.text === '/test') {
-          sendMessageMarkup(m.message.chat.id, 'Morgen ist ' + appointments[0].name + '. Steht die Mülltonne schon draußen?',
-            '{"keyboard": [["Müll steht draußen"], ["Erneut erinnern in 10 Minuten"], ["Erneut erinnern in 2 Stunden"]],"one_time_keyboard": true}'
-          )
-        } else if (m.message.text === 'Müll steht draußen' || m.message.text.toLowerCase() === 'ja') {
-          var openAppointments = false
-          var now = new Date()
-          var future = new Date()
-          future.setHours(now.getHours() + hoursPrev)
-
-          for (var a in appointments) {
-            if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
-              openAppointments = true
-            }
-          }
-          if (openAppointments) {
-            sendMessage(m.message.chat.id, 'Sehr gut, vielen Dank.')
-
-            for (var a in appointments) {
-              if (appointments[a].start > now && appointments[a].start < future) {
-                appointments[a].todo = false
-              }
-            }
-
-            for (var u in users) {
-              if (u !== m.message.chat.id) {
-                sendMessage(u, users[m.message.chat.id].name + ' hat den Müll rausgestellt!')
-              }
-            }
-          } else {
-            sendMessage(m.message.chat.id, 'Derzeit stehen keine Termine an.')
-          }
-        } else if (startsWith(m.message.text, 'Erneut erinnern in') || m.message.text.toLowerCase() === 'nein') {
-          var openAppointments = false
-          var now = new Date()
-          var future = new Date()
-          future.setHours(now.getHours() + hoursPrev)
-          for (var a in appointments) {
-            if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
-              openAppointments = true
-            }
-          }
-          if (openAppointments) {
-            sendMessage(m.message.chat.id, 'Okay, ich erinnere dich erneut in einer Stunde.')
-          } else {
-            sendMessage(m.message.chat.id, 'Derzeit stehen keine Termine an.')
-          }
-        } else {
-          sendMessage(m.message.chat.id, 'Sorry, I could not understand message: ' + m.message.text)
-        }
-      }
+      messagesRespond(updates)
     }
 
-    // send outgoing messages
-    var now = new Date()
-    var future = new Date()
-    future.setHours(now.getHours() + hoursPrev)
-    // get appointments less then 6 hours in the future
+    messagesGenerate()
 
-    for (var a in appointments) {
-      if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
-        if (appointments[a].reminderXh > ((appointments[a].start - now)/3600000)) {
-          appointments[a].reminderXh--
-
-          for (var i in users) {
-            log(0, 'sending reminder to user ' + i + ' about ' + appointments[a].name)
-            sendMessageMarkup(i, 'Morgen ist ' + appointments[a].name + '. Steht die Mülltonne schon draußen?',
-              '{"keyboard": [["Müll steht draußen"], ["Erneut erinnern in einer Stunde"]],"one_time_keyboard": true}')
-          }
-        }
-      }
-    }
+    // call function again after 2s
     setTimeout(getUpdates(updateID + 1), 2000)
   })
+}
+
+// respond to incomming messages
+function messagesRespond (updates) {
+  for (var i in updates.result) {
+    var m = updates.result[i]
+
+    if (m.update_id > updateID) {
+      updateID = m.update_id
+    }
+    var message = m.message
+
+    log(message.chat.id, message.date + ' ' + message.from.first_name +
+      '(' + message.from.id + ')@' + message.chat.first_name + '(' +
+      message.chat.id + '): ' + message.text)
+
+    if (m.message.text === undefined) {
+      log(message.chat.id, util.inspect(message))
+      sendMessage(m.message.chat.id, 'Sorry, I could not understand message.')
+    } else if (m.message.text === '/kalender') {
+      sendMessage(m.message.chat.id, 'Die Termine der nächsten zwei Wochen sind:\n' + printList(getNextDays(14)))
+    } else if (m.message.text === '/start') {
+      if (users[m.message.chat.id]) {
+        sendMessage(m.message.chat.id, 'Du stehst bereits auf der Nutzerliste. Um von der Liste entfernt zu werden sende /stop')
+      } else {
+        sendMessage(m.message.chat.id, 'Du wurdest zur Nutzerliste hinzugefügt.')
+        fs.appendFile('config/wastebot.users', m.message.chat.id + '\n', function (err) {
+          if (err !== null) {
+            console.log('error3:' + err)
+          }
+        })
+        fs.writeFile('config/' + m.message.chat.id + '.username', m.message.chat.first_name, function (err) {
+          if (err !== null) {
+            console.log('error4:' + err)
+          }
+        })
+
+        var newUser = {}
+        newUser.name = m.message.chat.first_name
+        users[m.message.chat.id] = newUser
+      }
+    } else if (m.message.text === '/stop') {
+      if (users[m.message.chat.id]) {
+        sendMessage(m.message.chat.id, 'Du wurdest von der Nutzerliste entfernt. Um wieder als Nutzer hinzugefügt zu werden sende /start')
+        var usersNew = {}
+        for (var a in users) {
+          if (a !== m.message.chat.id) {
+            usersNew[a] = users[a]
+          }
+        }
+        users = usersNew
+
+        var userText = ''
+        for (var a2 in users) {
+          userText += a2 + '\n'
+        }
+
+        fs.writeFile('config/wastebot.users', userText, function (err) {
+          console.log('error5:' + err)
+        })
+      } else {
+        sendMessage(m.message.chat.id, 'Du stehst nicht auf der Nutzerliste. Um als Nutzer hinzugefügt zu werden sende /start')
+      }
+    } else if (m.message.text === '/test') {
+      sendMessageMarkup(m.message.chat.id, 'Morgen ist ' + appointments[0].name + '. Steht die Mülltonne schon draußen?',
+        '{"keyboard": [["Müll steht draußen"], ["Erneut erinnern in 10 Minuten"], ["Erneut erinnern in 2 Stunden"]],"one_time_keyboard": true}'
+      )
+    } else if (m.message.text === 'Müll steht draußen' || m.message.text.toLowerCase() === 'ja') {
+      var openAppointments = false
+      var now = new Date()
+      var future = new Date()
+      future.setHours(now.getHours() + hoursPrev)
+
+      for (var a in appointments) {
+        if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
+          openAppointments = true
+        }
+      }
+      if (openAppointments) {
+        sendMessage(m.message.chat.id, 'Sehr gut, vielen Dank.')
+
+        for (var a in appointments) {
+          if (appointments[a].start > now && appointments[a].start < future) {
+            appointments[a].todo = false
+          }
+        }
+
+        for (var u in users) {
+          if (u !== m.message.chat.id) {
+            sendMessage(u, users[m.message.chat.id].name + ' hat den Müll rausgestellt!')
+          }
+        }
+      } else {
+        sendMessage(m.message.chat.id, 'Derzeit stehen keine Termine an.')
+      }
+    } else if (startsWith(m.message.text, 'Erneut erinnern in') || m.message.text.toLowerCase() === 'nein') {
+      var openAppointments = false
+      var now = new Date()
+      var future = new Date()
+      future.setHours(now.getHours() + hoursPrev)
+      for (var a in appointments) {
+        if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
+          openAppointments = true
+        }
+      }
+      if (openAppointments) {
+        sendMessage(m.message.chat.id, 'Okay, ich erinnere dich erneut in einer Stunde.')
+      } else {
+        sendMessage(m.message.chat.id, 'Derzeit stehen keine Termine an.')
+      }
+    } else {
+      sendMessage(m.message.chat.id, 'Sorry, I could not understand message: ' + m.message.text)
+    }
+  }
+}
+
+// send outgoing messages
+function messagesGenerate () {
+  var now = new Date()
+  var future = new Date()
+  future.setHours(now.getHours() + hoursPrev)
+  // get appointments less then 6 hours in the future
+
+  for (var a in appointments) {
+    if (appointments[a].todo && appointments[a].start > now && appointments[a].start < future) {
+      if (appointments[a].reminderXh > ((appointments[a].start - now) / 3600000)) {
+        appointments[a].reminderXh--
+
+        for (var i in users) {
+          log(0, 'sending reminder to user ' + i + ' about ' + appointments[a].name)
+          sendMessageMarkup(i, 'Morgen ist ' + appointments[a].name + '. Steht die Mülltonne schon draußen?',
+            '{"keyboard": [["Müll steht draußen"], ["Erneut erinnern in einer Stunde"]],"one_time_keyboard": true}')
+        }
+      }
+    }
+  }
 }
 
 getUpdates()
